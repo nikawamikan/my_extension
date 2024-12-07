939801ebe5150a22d7595aaf02a335279df47d3c
nikawamikan <wkwktktkkrkr@outlook.jp>
2024-12-07 12:04:53 +0000

    update: 'helloWorld'コマンドを'destroy'に変更し、WebViewを追加

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,81 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as childProcess from 'child_process';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "my-extension" is now active!');
+	// Destroyコマンドを登録
+	const disposable = vscode.commands.registerCommand('my-extension.destroy', () => {
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('my-extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from my_extension!');
+		// WebViewを開く
+		const panel = vscode.window.createWebviewPanel(
+			'destroy',
+			'Destroy',
+			vscode.ViewColumn.One,
+			{
+				enableScripts: true
+			}
+		);
+
+		// WebViewに表示するHTMLを生成
+		panel.webview.html = getWebviewContent();
+
+
+		// WebViewからのメッセージを受信
+		panel.webview.onDidReceiveMessage(
+			message => {
+				switch (message.command) {
+					case 'destroy':
+						// consoleのコマンドを実行する
+						const command = "echo 'Destroy'";
+
+						// コマンドを実行
+						childProcess.exec(command, (error, stdout, stderr) => {
+							if (error) {
+								console.error(`exec error: ${error}`);
+								return;
+							}
+							if (stdout) {
+								console.log(`stdout: ${stdout}`);
+								return;
+							}
+						});
+						return;
+
+				}
+			},
+			undefined,
+			context.subscriptions
+		);
+
 	});
 
 	context.subscriptions.push(disposable);
+}
+
+// WebViewに表示するHTMLを生成（これが文字列なのでちょっとメンドイ）
+function getWebviewContent() {
+	return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Cat Coding</title>
+    </head>
+    <body>
+        <h1>Destroy</h1>
+        <button onclick="destroy()">Destroy</button>
+
+        <script>
+            const vscode = acquireVsCodeApi();
+
+            function destroy() {
+                vscode.postMessage({
+                    command: 'destroy'
+                });
+            }
+        </script>
+    </body>
+    </html>`;
 }
 
 // This method is called when your extension is deactivated
